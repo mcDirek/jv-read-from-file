c1df959a8446a63a898ca1fbf42226e2731a3ae6
mcDirek <myehan9@gmail.com>
2025-10-23 13:55:54 +0200

    File Work

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
